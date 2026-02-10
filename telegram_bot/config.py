3077f63026d301fa726e00c25d4f13f182ed476d
crazypumkin70 <65696227+crazypumkin70@users.noreply.github.com>
2026-02-10 20:04:22 +0800

    Create config.py

---
+++ telegram_bot/config.py
@@ -0,0 +1,9 @@
+# Telegram Bot Token
+TELEGRAM_TOKEN = "8570122961:AAGBsHFrJdXcTS337a_KOJMXkTm5i1y5ykA"
+
+# Update this with the ID you got from the /getid command!
+ALERT_CHAT_ID = -5177397842  
+
+# Firebase credentials
+SERVICE_ACCOUNT_FILE = "serviceAccountKey.json"
+DATABASE_URL = "https://ffiredetection-1673c-default-rtdb.asia-southeast1.firebasedatabase.app"
